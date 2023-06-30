ec7b990ef79bfd1f7a598ad9c7f77333d4a4493d
Kapil-Vasanth <118164273+Kapil-Vasanth@users.noreply.github.com>
2023-06-30 16:00:48 +0530

    testing netlify login form

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -63,7 +63,20 @@ export default function Login() {
                     }
                 </button>
             </Form>
-
+            <div>
+            <form name="contact" netlify>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+        
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+        
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+        
+            <button type="submit">Submit</button>
+          </form> 
+            </div>            
         </div>
     )
 }
